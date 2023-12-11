e6bd4be81dd67832009600e24bd6c31c82b5798d
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2023-12-11 20:41:24 +1100

    initial set up including MVC and basic files

---
+++ server.js
@@ -0,0 +1,7 @@
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
