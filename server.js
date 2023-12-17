02f6a899efc5583440bdf596e6d6c78166787e5b
isaacmasterman <142137796+isaacmasterman@users.noreply.github.com>
2023-12-17 17:21:56 +1100

    buit out handlebars and styling

--- server.js
+++ server.js
@@ -1,7 +1,48 @@
+const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const session = require('express-session');
+// const routes = require('./controllers'); // Adjust the path as neede!!!
+const sequelize = require('./config/connection');
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials/'
+}));
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
+// Parse JSON and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+app.use(session({
+    secret: sessionSecret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}));
+
+//app.use('/', routes);
+
+sequelize.sync()
+  .then(() => {
+    console.log('Database synchronized successfully');
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error syncing the database:', err);
+  });
